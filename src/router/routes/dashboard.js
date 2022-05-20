ae99185582c90f49a7b51f6b02c6618665da1a39
Matheus Pedroni <mpedroni77@gmail.com>
2022-05-20 08:27:11 -0300

    Creates Agent dashboard view

--- src/router/routes/dashboard.js
+++ src/router/routes/dashboard.js
@@ -4,6 +4,11 @@ const routes = [
     name: 'dashboard.manager',
     component: () => import('@/views/Dashboard/Manager'),
   },
+  {
+    path: '/dashboard/agent',
+    name: 'dashboard.agent',
+    component: () => import('@/views/Dashboard/Agent'),
+  },
 ];
 
 export default routes;
---
+++ src/views/Dashboard/Agent/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <dashboard-layout>
+    <template #header> {{ agent.name }} </template>
+
+    <template #actions>
+      <dashboard-filters @filter="filters = $event" v-bind="{ tags, dates }" />
+    </template>
+
+    <history-metrics-by-agent :agentName="agent.name" />
+  </dashboard-layout>
+</template>
+
+<script>
+import DashboardLayout from '@/layouts/DashboardLayout';
+import DashboardFilters from '../components/Filters';
+import HistoryMetricsByAgent from '../Manager/metrics/ByAgent/HistoryMetrics';
+
+export default {
+  name: 'DashboardManager',
+
+  components: {
+    DashboardFilters,
+    DashboardLayout,
+    HistoryMetricsByAgent,
+  },
+
+  data: () => ({
+    agent: {
+      name: 'Juliano',
+    },
+    filters: {
+      tab: '',
+      date: '',
+    },
+    tags: [
+      { text: 'Dúvidas', value: 'doubts' },
+      { text: 'Financeiro', value: 'finance' },
+      { text: 'Ajuda', value: 'help' },
+    ],
+    dates: [
+      { value: '', text: 'Mês Atual' },
+      { value: 'last-7-days', text: 'Últimos 7 dias' },
+      { value: 'last-14-days', text: 'Últimos 14 dias' },
+      { value: 'last-30-days', text: 'Últimos 30 dias' },
+      { value: 'last-12-months', text: 'Últimos 12 meses' },
+      { value: 'all', text: 'Desde o início' },
+    ],
+  }),
+};
+</script>
