b62e2281a6d36e9eff6a2c58459feb76eafeb88d
Matheus Pedroni <mpedroni77@gmail.com>
2022-07-19 14:52:38 -0300

    feat: Add `logrocket` dependency and configs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue';
+import LogRocket from 'logrocket';
+
 import App from './App.vue';
 import i18n from './plugins/i18n';
 import router from './router';
 import store from './store';
+import env from './utils/env';
 
 import './styles/global.scss';
+
+LogRocket.init('lqshel/test-weni-webapp', {
+  mergeIframes: true,
+  parentDomain: env('VUE_APP_PARENT_DOMAIN'),
+});
 
 Vue.config.productionTip = false;
 
