4e3d3deaf8c7b8bee5e56ce3eef7fb1868ee2864
Matheus Pedroni <mpedroni77@gmail.com>
2022-07-29 15:32:50 -0300

    feat: Add mutations and actions in the Room Vuex module

--- src/store/modules/rooms.js
+++ src/store/modules/rooms.js
@@ -2,17 +2,22 @@ import Room from '@/services/api/resources/room';
 
 const mutations = {
   SET_ROOMS: 'SET_ROOMS',
+  SET_ACTIVE_ROOM: 'SET_ACTIVE_ROOM',
 };
 
 export default {
   namespaced: true,
   state: {
     rooms: [],
+    activeRoom: null,
   },
 
   mutations: {
     [mutations.SET_ROOMS](state, rooms) {
       state.rooms = rooms;
+    },
+    [mutations.SET_ACTIVE_ROOM](state, room) {
+      state.activeRoom = room;
     },
   },
 
@@ -25,6 +30,9 @@ export default {
 
       return rooms;
     },
+    setActiveRoom({ commit }, room) {
+      commit(mutations.SET_ACTIVE_ROOM, room);
+    },
   },
 
   getters: {
@@ -33,6 +41,9 @@ export default {
     },
     waitingQueue(state) {
       return state.rooms.filter((room) => !room.user);
+    },
+    getRoomById: (state) => (uuid) => {
+      return state.rooms.find((room) => room.uuid === uuid);
     },
   },
 };
