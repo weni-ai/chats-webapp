b4d9de249f1152244b3fde749b8526676decaa3a
Matheus Pedroni <mpedroni77@gmail.com>
2022-08-26 17:56:28 -0300

    fix: Change the Authorization request header for all environments

--- src/services/api/http.js
+++ src/services/api/http.js
@@ -6,12 +6,10 @@ const client = axios.create({
   baseURL: `https://${env('CHATS_API_URL')}/v1`,
 });
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 client.interceptors.request.use((config) => {
   const token = get();
   // eslint-disable-next-line no-param-reassign
-  if (token) config.headers.Authorization = `${isProduction ? 'Bearer' : 'Token'} ${token}`;
+  if (token) config.headers.Authorization = `Token ${token}`;
   return config;
 });
 
