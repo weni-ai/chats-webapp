4741cd5b232467a86ea421c1374983db70e6a470
Matheus Pedroni <mpedroni77@gmail.com>
2022-09-27 12:00:05 -0300

    feat: Add notification on room create

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,38 @@
 <script>
 import { ws } from '@/services/api/socket';
 
+class Notification {
+  /**
+   * @type {HTMLAudioElement}
+   */
+  #notification;
+
+  constructor(soundName, type = 'wav') {
+    this.#notification = new Audio(`notifications/${soundName}.${type}`);
+  }
+
+  notify() {
+    // if the user hadn't interacted with the page yet (click, scroll...),
+    // the browser blocks the audio playing because is considered autoplay media
+    this.#notification.play().catch(() => {});
+  }
+}
+
 export default {
   name: 'App',
 
   created() {
     ws.on('msg.create', (message) => {
-      if (!this.activeRoom || this.activeRoom.user?.uuid !== message.user?.uuid)
+      if (!this.activeRoom || this.activeRoom.user?.uuid !== message.user?.uuid) {
         this.$store.dispatch('rooms/addMessage', message);
+        const notification = new Notification('ping-bing');
+        notification.notify();
+      }
+    });
+
+    ws.on('room.create', () => {
+      const notification = new Notification('select-sound');
+      notification.notify();
     });
 
     ws.on('msg.update', (message) => {
