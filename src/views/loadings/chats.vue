b0e7e3c3449a1bc4cfaf918c7baeb30fce2322ab
Vanessa Elizabeth Piccoli <vanepiccoli7@gmail.com>
2022-11-03 13:28:28 -0300

    Add new skeleton layout

--- src/views/loadings/chats.vue
+++ src/views/loadings/chats.vue
@@ -1,39 +1,46 @@
 <template>
-  <div class="page">
-    <div class="unnnic-grid-giant">
-      <!-- <div class="unnnic-grid-span-12">
-        <unnnic-skeleton-loading height="104px" />
-      </div> -->
-      <div class="unnnic-grid-span-4">
-        <unnnic-skeleton-loading tag="div" height="104px" />
+  <div class="side-bar">
+    <div class="unnnic-grid-giant" style="padding: 12px 24px">
+      <div class="unnnic-grid-span-3" style="display: flex; flex-direction: column">
+        <unnnic-skeleton-loading height="20px" width="90px" />
+        <div style="margin-top: 10px"><unnnic-skeleton-loading height="45px" width="350px" /></div>
       </div>
-
-      <div v-for="n in 3" :key="n" class="unnnic-grid-span-4">
-        <div class="dash-loading-card">
-          <unnnic-skeleton-loading tag="div" width="40px" height="40px" />
-          <unnnic-skeleton-loading tag="div" height="40px" />
-        </div>
-      </div>
-
-      <div class="unnnic-grid-span-4">
-        <unnnic-skeleton-loading
-          v-for="n in 3"
-          tag="div"
-          height="107px"
-          :key="n"
-          style="margin-bottom: 1rem"
-        />
-      </div>
-      <div class="unnnic-grid-span-4">
-        <unnnic-skeleton-loading
-          v-for="n in 4"
-          tag="div"
-          height="93px"
-          :key="n"
-          style="margin-bottom: 1rem"
-        />
+      <div class="unnnic-grid-span-9">
+        <unnnic-skeleton-loading height="45px" />
       </div>
     </div>
+    <div class="unnnic-grid-giant">
+      <div class="unnnic-grid-span-3">
+        <unnnic-skeleton-loading height="45px" width="350px" />
+      </div>
+      <div class="unnnic-grid-span-2">
+        <unnnic-skeleton-loading height="40px" width="200px" />
+      </div>
+      <div class="unnnic-grid-span-2">
+        <unnnic-skeleton-loading height="40px" width="200px" />
+      </div>
+      <div class="unnnic-grid-span-2">
+        <unnnic-skeleton-loading height="40px" width="200px" />
+      </div>
+      <div class="unnnic-grid-span-2">
+        <unnnic-skeleton-loading height="40px" width="200px" />
+      </div>
+    </div>
+    <div class="unnnic-grid-giant">
+      <div class="unnnic-grid-span-1" style="display: flex; flex-direction: column">
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="20px" width="90px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+        <div class="skeleton-line"><unnnic-skeleton-loading height="45px" width="350px" /></div>
+      </div>
+    </div>
+    <div class="page"></div>
   </div>
 </template>
 
@@ -44,24 +51,26 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.dash-loading-card {
-  display: flex;
-  > div:first-child {
-    margin-right: 1rem;
-  }
-  div:last-child {
-    width: 100%;
-  }
-}
-
-.page {
+.side-bar {
   position: absolute;
   left: 0%;
   right: 0%;
   top: 0%;
   bottom: 0.14%;
-  background: linear-gradient(90deg, #e8f4f4 0%, #ffffff 100%);
+  background-color: #ffffff;
   opacity: 2;
   border-radius: 4px;
+}
+.page {
+  background: linear-gradient(90deg, #e8f4f4 0%, #ffffff 100%);
+  position: absolute;
+  width: 50%;
+  height: 80%;
+  left: 400px;
+  top: 165px;
+}
+
+.skeleton-line {
+  padding: 7px 0px;
 }
 </style>
