17035e87a8fbc43a5695e95e9a362c30bd736107
Vanessa Elizabeth Piccoli <vanepiccoli7@gmail.com>
2022-12-21 14:58:20 -0300

    new quick message template

--- src/layouts/ChatsLayout/index.vue
+++ src/layouts/ChatsLayout/index.vue
@@ -1,16 +1,26 @@
 <template>
-  <section
-    class="chats-layout unnnic-grid-giant"
-    style="padding: 0px 0px; padding-left: 10px; overflow-y: hidden"
-  >
-    <slot name="room-list" v-if="!this.contactList">
+  <section class="chats-layout unnnic-grid-giant" style="padding: 0px; overflow-y: hidden">
+    <section
+      v-if="isAsideSlotInUse && this.showQuickMessage && !this.showQuickMessagePreferencesBar"
+      class="aside unnnic-grid-span-3"
+      style="border: 1px solid #e2e6ed"
+    >
+      <slot name="aside" />
+    </section>
+    <slot
+      name="room-list"
+      v-if="!this.contactList && !this.showQuickMessage && !this.showQuickMessagePreferencesBar"
+    >
       <div
-        :style="{ display: 'flex', flexDirection: 'column', height: '100vh' }"
+        :style="{ display: 'flex', flexDirection: 'column', height: '100vh', paddingLeft: '10px' }"
         class="unnnic-grid-span-3"
       >
-        <preferences-bar :style="{ margin: '16px 0 0 0px' }" />
+        <preferences-bar
+          :style="{ margin: '16px 0 0 0px' }"
+          @show-quick-messages="preferencesOpenQuickMessage"
+        />
 
-        <div class="template-message-button" v-if="canTriggerFlows">
+        <div class="template-message-button">
           <unnnic-button-icon
             size="small"
             icon="pencil-write-1"
@@ -23,24 +33,40 @@
       </div>
     </slot>
 
-    <slot name="template-message" v-if="this.contactList">
+    <slot
+      name="template-message"
+      v-if="this.contactList && !this.showQuickMessage && !this.showQuickMessagePreferencesBar"
+    >
       <div
-        :style="{ display: 'flex', flexDirection: 'column', height: '100vh' }"
+        :style="{ display: 'flex', flexDirection: 'column', height: '100vh', paddingLeft: '10px' }"
         class="unnnic-grid-span-3"
       >
         <contact-list class="room-list" :disabled="disabledChatList" @close="closeContactList" />
       </div>
     </slot>
+    <slot
+      name="quick-message"
+      v-if="!this.contactList && !this.showQuickMessage && this.showQuickMessagePreferencesBar"
+    >
+      <div
+        :style="{ display: 'flex', flexDirection: 'column', height: '100vh' }"
+        class="unnnic-grid-span-3"
+      >
+        <quick-messages class="room-list" @close="closeQuickMessages" />
+      </div>
+    </slot>
 
     <main
-      v-bind:class="[isAsideSlotInUse ? 'unnnic-grid-span-6' : 'unnnic-grid-span-9']"
+      v-bind:class="[
+        isAsideSlotInUse && !this.showQuickMessage ? 'unnnic-grid-span-6' : 'unnnic-grid-span-9',
+      ]"
       style="height: 100vh"
     >
       <slot />
     </main>
 
     <section
-      v-if="isAsideSlotInUse"
+      v-if="isAsideSlotInUse && !this.showQuickMessage"
       class="aside unnnic-grid-span-3"
       style="border: 1px solid #e2e6ed"
     >
@@ -61,6 +87,7 @@ import PreferencesBar from '@/components/PreferencesBar.vue';
 import Sector from '@/services/api/resources/settings/sector.js';
 import TemplateMessages from '@/services/api/resources/chats/templateMessage.js';
 import SkeletonLoading from '@/views/loadings/chats.vue';
+import QuickMessages from '@/components/chats/QuickMessages';
 import TheRoomList from './components/TheRoomList';
 import ContactList from './components/TemplateMessages';
 
@@ -73,6 +100,7 @@ export default {
     // ModalOnBoardingChats,
     SkeletonLoading,
     ContactList,
+    QuickMessages,
   },
 
   props: {
@@ -84,10 +112,17 @@ export default {
   },
   mounted() {
     this.getCountSectors();
-    this.havePermissionToSendTemplateMessage();
+    // this.havePermissionToSendTemplateMessage();
   },
 
   methods: {
+    preferencesOpenQuickMessage() {
+      this.showQuickMessagePreferencesBar = true;
+    },
+    closeQuickMessages() {
+      this.showQuickMessagePreferencesBar = false;
+    },
+
     showContactsList() {
       this.contactList = true;
     },
@@ -122,10 +157,21 @@ export default {
     isLoading: false,
     contactList: false,
     canTriggerFlows: false,
+    showQuickMessage: false,
+    openQuickMessage: false,
+    teste: false,
+    showQuickMessagePreferencesBar: false,
   }),
 
   computed: {
     isAsideSlotInUse() {
+      if (![null, undefined, ''].includes(this.$slots.aside)) {
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        this.showQuickMessage = this.$slots.aside[0].componentOptions.tag === 'QuickMessages';
+      } else {
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        this.showQuickMessage = false;
+      }
       return !!this.$slots.aside;
     },
   },
