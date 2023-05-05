124237b08cefad0c6d4a6923e5e8bce0f7175881
Vanessa Elizabeth Piccoli <vanepiccoli7@gmail.com>
2023-05-05 09:48:21 -0300

    removed space

--- src/views/chats/ActiveChat.vue
+++ src/views/chats/ActiveChat.vue
@@ -255,7 +255,6 @@ export default {
       this.componentInAsideSlot = '';
       this.page = 0;
       this.readMessages();
-      
     },
     async getRoomMessages(concat) {
       this.isLoading = true;
