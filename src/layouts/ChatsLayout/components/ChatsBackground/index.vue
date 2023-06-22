2edd0f84e0fd9ddb37420c1adad691cfdff60ab7
aldemylla <aldemylla@gmail.com>
2023-06-22 16:08:26 -0300

    fix: Chats background

--- src/layouts/ChatsLayout/components/ChatsBackground/index.vue
+++ src/layouts/ChatsLayout/components/ChatsBackground/index.vue
@@ -28,6 +28,7 @@ export default {
     &__background {
       position: absolute;
       height: 100%;
+      width: 100%;
       object-fit: cover;
       object-position: center;
       pointer-events: none;
