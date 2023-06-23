243bdecc8b5bbff75e6c33fe1fbd92409134fe7e
aldemylla <aldemylla@gmail.com>
2023-06-23 09:09:56 -0300

    feat: Added TextBox tests

--- src/components/chats/MessageEditor/__tests__/TextBox.spec.js
+++ src/components/chats/MessageEditor/__tests__/TextBox.spec.js
@@ -1,5 +1,8 @@
-import { mount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
+import { unnnicButtonIcon } from '@weni/unnnic-system';
 import TextBox from '../TextBox.vue';
+
+const localVue = createLocalVue();
 
 function createWrapper(props = {}) {
   const wrapper = mount(TextBox, {
@@ -8,6 +11,10 @@ function createWrapper(props = {}) {
       onKeyDown: jest.fn(),
       ...props,
     },
+    stubs: {
+      unnnicButtonIcon,
+    },
+    localVue,
   });
 
   return wrapper;
@@ -41,60 +48,26 @@ describe('TextBox', () => {
   });
 
   it('should clears the textarea when send message', async () => {
-    const wrapper = createWrapper({ value: 'Hello' });
+    const wrapper = createWrapper();
     const textarea = wrapper.find('textarea');
 
+    textarea.setValue('Hello');
     expect(textarea.element.value).toBe('Hello');
 
-    await textarea.trigger('focus');
-    await wrapper.setData({ value: 'Hello World' }); // Simular a entrada de um valor não vazio
-    await textarea.trigger('keydown.enter');
+    await wrapper.find('.button-icon.secondary').trigger('click');
 
     expect(textarea.element.value).toBe('');
   });
 
-  // it('adjusts the textarea height when the content changes', async () => {
-  //   const wrapper = createWrapper();
-  //   const textarea = wrapper.find('textarea');
+  it('should adjusts the textarea height when the content changes', async () => {
+    const wrapper = createWrapper();
+    const textarea = wrapper.find('textarea');
 
-  //   const lineHeight = parseFloat(getComputedStyle(textarea.element).lineHeight);
-  //   const maxHeight = wrapper.vm.maxTextareaRows * lineHeight;
+    await textarea.setValue('Line 1\nLine 2\nLine 3\nLine 4\nLine 5\nLine 6');
+    expect(textarea.element.style.height).not.toBe('auto');
 
-  //   // Enter enough text to exceed the maximum height
-  //   textarea.setValue('Line 1\nLine 2\nLine 3\nLine 4\nLine 5\nLine 6\nLine 7');
-  //   await wrapper.vm.$nextTick();
+    await textarea.setValue('');
 
-  //   expect(textarea.element.style.height).toBe(`${maxHeight}px`);
-
-  //   // Clear the text and check if the height resets
-  //   textarea.setValue('');
-  //   await wrapper.vm.$nextTick();
-
-  //   expect(textarea.element.style.height).toBe('auto');
-  // });
-
-  // it('emits a send event when the send button is clicked', async () => {
-  //   const wrapper = createWrapper();
-  //   const sendButton = wrapper.find('.actions unnnic-button-icon[type="secondary"]');
-
-  //   sendButton.trigger('click');
-  //   await wrapper.vm.$nextTick();
-
-  //   expect(wrapper.emitted('send')).toBeTruthy();
-  // });
-
-  // it('does not emit a send event when Shift + Enter is pressed', async () => {
-  //   const wrapper = createWrapper();
-  //   const textarea = wrapper.find('textarea');
-
-  //   textarea.setValue('Hello');
-  //   await wrapper.vm.$nextTick();
-
-  //   expect(wrapper.emitted('send')).toBeFalsy();
-
-  //   textarea.trigger('keydown', { key: 'Enter', shiftKey: true });
-  //   await wrapper.vm.$nextTick();
-
-  //   expect(wrapper.emitted('send')).toBeFalsy();
-  // });
+    expect(textarea.element.style.height).toBe('auto');
+  });
 });
