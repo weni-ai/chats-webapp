c17dcf1a1bfdae19bdbd36a5ead5b3cab0de37fd
Matheus Cristian <mathues@email.com>
2023-08-02 14:55:44 -0300

    feat: replaced native window notification to iframessa package to emit notifications to connect

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,31 +1,21 @@
 import i18n from '@/plugins/i18n';
 import logo from '../assets/weni-logo.svg';
+import iframessa from 'iframessa';
 
-/* eslint-disable no-new */
-function supportsWindowNotification() {
-  if (!('Notification' in window)) {
-    console.error('This browser does not support desktop notification');
-    return false;
-  }
-
-  return true;
-}
+iframessa.register('chats');
 
 export function sendWindowNotification({ title = 'Notification', message = '', image = '' }) {
-  if (Notification.permission === 'granted' && supportsWindowNotification()) {
-    const options = {
-      silent: true,
-      badge: logo,
-      icon: logo,
-      body: image ? `${i18n.t('media')}\n${message}` : message,
-      tag: title,
-    };
-    new Notification(title, options);
-  }
+  const options = {
+    silent: true,
+    badge: logo,
+    icon: logo,
+    body: image ? `${i18n.t('media')}\n${message}` : message,
+    tag: title,
+  };
+
+  iframessa.emit('notification', [title, options]);
 }
 
 export function requestPermission() {
-  if (supportsWindowNotification && Notification.permission !== 'granted') {
-    Notification.requestPermission();
-  }
+  iframessa.emit('notification.requestPermission');
 }
