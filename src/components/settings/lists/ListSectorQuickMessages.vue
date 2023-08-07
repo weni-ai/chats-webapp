c95819b8517ba145db327fb31de9159cf9b6ebd8
Aldemylla <aldemylla@gmail.com>
2023-08-07 15:40:07 -0300

    feat: Added message when without quick messages

--- src/components/settings/lists/ListSectorQuickMessages.vue
+++ src/components/settings/lists/ListSectorQuickMessages.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="list-sector-quick-messages">
     <p v-if="sector" class="title">{{ $t('quick_messages.title_by_sector', { sector }) }}</p>
+    <p v-if="quickMessagesShared.length === 0" class="without-messages">
+      {{ $t('quick_messages.without_messages.start') }}
+      <button @click="$emit('create-quick-message')">
+        {{ $t('quick_messages.without_messages.middle') }}
+      </button>
+      {{ $t('quick_messages.without_messages.end') }}
+    </p>
 
     <quick-message-card
       v-for="message in quickMessagesShared"
@@ -46,6 +53,23 @@ export default {
     line-height: 1.5rem;
 
     margin-bottom: 1rem;
+  }
+
+  .without-messages {
+    font-size: $unnnic-font-size-body-gt;
+    color: $unnnic-color-neutral-dark;
+
+    button {
+      background: none;
+      border: none;
+      padding: 0;
+
+      text-decoration: underline;
+      font-weight: $unnnic-font-weight-bold;
+      color: $unnnic-color-neutral-dark;
+
+      cursor: pointer;
+    }
   }
 
   .quick-message-card {
