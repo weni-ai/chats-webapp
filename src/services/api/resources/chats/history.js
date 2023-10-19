6937fd8e589ed8c92cc8c074c3a31baa6dae47df
aldemylla <aldemylla@gmail.com>
2023-10-19 18:41:03 -0300

    fix: Removed commented code on history api resource

--- src/services/api/resources/chats/history.js
+++ src/services/api/resources/chats/history.js
@@ -2,29 +2,6 @@ import http from '@/services/api/http';
 import { getProject } from '@/utils/config';
 
 export default {
-  // async getAllWithClosedRooms(offset, limit, contact, date, tags, sector) {
-  //   const response = await http.get('/contact/', {
-  //     params: {
-  //       project: getProject(),
-  //       offset,
-  //       limit,
-  //       search: contact,
-  //       r_ended_at_after: date.start,
-  //       r_ended_at_before: date.end,
-  //       tag: tags,
-  //       sector,
-  //     },
-  //   });
-  //   return response.data;
-  // },
-  // async getUnicContactClosedRooms(contact) {
-  //   const response = await http.get(`/contact/${contact}`, {
-  //     params: {
-  //       project: getProject(),
-  //     },
-  //   });
-  //   return response.data;
-  // },
   async getHistoryRooms({
     offset = 0,
     limit = 5,
