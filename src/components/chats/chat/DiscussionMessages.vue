3a77ecc41d459dae4ca7ee5182ce95e59c5aee1a
aldemylla <aldemylla@gmail.com>
2023-11-23 10:55:30 -0300

    feat: Resending messages and medias of discussion

--- src/components/chats/chat/DiscussionMessages.vue
+++ src/components/chats/chat/DiscussionMessages.vue
@@ -6,7 +6,6 @@
     :messagesSendingUuids="discussionMessagesSendingUuids"
     :messagesFailedUuids="discussionMessagesFailedUuids"
     :resendMessages="discussionResendMessages"
-    :resendMessage="discussionResendMessage"
     :resendMedia="discussionResendMedia"
     :showChatSeparator="false"
     :isLoading="isLoading"
@@ -49,9 +48,8 @@ export default {
 
   methods: {
     ...mapActions({
-      discussionResendMessages: 'chats/discussionMessages/resendMessages',
-      discussionResendMessage: 'chats/discussionMessages/discussionResendMessage',
-      discussionResendMedia: 'chats/discussionMessages/resendMedia',
+      discussionResendMessages: 'chats/discussionMessages/resendDiscussionMessages',
+      discussionResendMedia: 'chats/discussionMessages/resendDiscussionMedia',
     }),
 
     async getDiscussionMessages() {
