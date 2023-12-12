0a8b9ba55b79c342b35e2d533abbcb05c4c01e5f
aldemylla <aldemylla@gmail.com>
2023-12-12 17:30:15 -0300

    fix: Revert flows trigger search contacts requitision filter

--- src/services/api/resources/chats/flowsTrigger.js
+++ src/services/api/resources/chats/flowsTrigger.js
@@ -8,7 +8,7 @@ export default {
     const response = await http.get(`/project/${getProject()}/list_contacts/`, {
       params: {
         cursor: next,
-        search,
+        name: search,
       },
     });
     return response.data;
