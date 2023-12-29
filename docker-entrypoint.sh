4911e3d508b8c152181cba5fbdfdf01660baf4b9
nataliaweni <natalia.assis@weni.ai>
2023-12-29 15:40:43 -0300

    Update dockerfile, entrypoint and nginx.conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,6 @@ export JSON_STRING='window.configs = { \
   "VUE_APP_LOGROCKET_PARENT_DOMAIN":"'${VUE_APP_LOGROCKET_PARENT_DOMAIN}'", \
   "VUE_APP_BOT_URL":"'${VUE_APP_BOT_URL}'", \
 }'
-sed -i "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/index.html
+sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/index.html.tmpl > /tmp/index.html
 
 exec "$@"
