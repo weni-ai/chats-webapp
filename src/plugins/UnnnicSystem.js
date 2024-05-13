531d84f39c807fdf4eece9fb6d390dbd9e2bc167
aldemylla <aldemylla@gmail.com>
2024-05-13 15:51:38 -0300

    fix: Updated fingerprints at assetlinks

--- src/plugins/UnnnicSystem.js
+++ src/plugins/UnnnicSystem.js
@@ -1,5 +1,7 @@
 import * as UnnnicSystem from '@weni/unnnic-system';
 
+console.log('UnnnicSystem', UnnnicSystem);
+
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
