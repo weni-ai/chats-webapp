3d44475c5213d54df37fceeaea8891172329a5a7
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-06-21 11:57:10 -0300

    fix: update pwa config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,10 +3,36 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logos/weni-512.png'],
+      manifest: {
+        name: 'Weni Chats',
+        short_name: 'Weni',
+        theme_color: '#00A49F',
+        icons: [
+          {
+            src: 'logos/weni-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        swSrc: './src/service-worker.js',
+        injectManifest: {
+          swSrc: './src/service-worker.js',
+        },
+      },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
