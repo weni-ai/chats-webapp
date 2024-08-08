2e096b1f23e662f95c5619a20ccc8bbad5f10f78
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-08-08 14:58:43 -0300

    feat: add transfer room to another user test

--- src/store/modules/chats/__tests__/rooms.spec.js
+++ src/store/modules/chats/__tests__/rooms.spec.js
@@ -6,6 +6,7 @@ import { useRooms } from '@/store/modules/chats/rooms';
 import { useProfile } from '@/store/modules/profile';
 
 import { roomsMock } from './mocks/roomsMock';
+
 import { mockProfileHumanServiceState } from '../../__tests__/mocks/profileMock';
 import { mockProfileAdminState } from '../../__tests__/mocks/profileMock';
 
@@ -27,7 +28,7 @@ vi.mock('@/store/modules/profile', () => ({
 }));
 
 describe('State Rooms', () => {
-  const findRoomByUuid = (roomsStore, uuid) => {
+  const existRoomByUuid = (roomsStore, uuid) => {
     return !!roomsStore.rooms.find((room) => room.uuid === uuid);
   };
 
@@ -51,7 +52,24 @@ describe('State Rooms', () => {
         { uuid: '3', queue: { uuid: '1' } },
         { app: { ...adminProfileStore, viewedAgent } },
       );
-      expect(findRoomByUuid(adminRoomsStore, '3')).eq(true);
+      expect(existRoomByUuid(adminRoomsStore, '3')).eq(true);
+    });
+
+    it('should view room because is seeing an agent of room', async () => {
+      await updateRoom(
+        {
+          uuid: '3',
+          queue: { uuid: '1' },
+          user: { email: 'testing@weni.ai' },
+        },
+        {
+          app: {
+            ...adminProfileStore,
+            viewedAgent: { email: 'testing@weni.ai' },
+          },
+        },
+      );
+      expect(existRoomByUuid(adminRoomsStore, '3')).eq(true);
     });
   });
 
@@ -71,21 +89,25 @@ describe('State Rooms', () => {
     });
 
     it('should remove room because user not in queue', async () => {
-      expect(findRoomByUuid(humanServiceRoomsStore, '1')).eq(true);
+      expect(existRoomByUuid(humanServiceRoomsStore, '1')).eq(true);
       await updateRoom(
         { uuid: '1', queue: { uuid: '3' } },
         { app: { ...humanServiceProfileStore, viewedAgent } },
       );
-      expect(findRoomByUuid(humanServiceRoomsStore, '1')).eq(false);
+      expect(existRoomByUuid(humanServiceRoomsStore, '1')).eq(false);
     });
 
     it('should view room because transfer user me', async () => {
-      expect(findRoomByUuid(humanServiceRoomsStore, '3')).eq(false);
+      expect(existRoomByUuid(humanServiceRoomsStore, '3')).eq(false);
       await updateRoom(
-        { uuid: '3', queue: { uuid: '3' }, user: { email: 'testing@weni.ai' } },
+        {
+          uuid: '3',
+          queue: { uuid: '3' },
+          user: { email: 'testing@weni.ai' },
+        },
         { app: { ...humanServiceProfileStore, viewedAgent } },
       );
-      expect(findRoomByUuid(humanServiceRoomsStore, '3')).eq(true);
+      expect(existRoomByUuid(humanServiceRoomsStore, '3')).eq(true);
     });
 
     it('should not view room because i dont have an active queue', async () => {
@@ -93,7 +115,35 @@ describe('State Rooms', () => {
         { uuid: '3', queue: { uuid: '2' } },
         { app: { ...humanServiceProfileStore, viewedAgent } },
       );
-      expect(findRoomByUuid(humanServiceRoomsStore, '3')).eq(false);
+      expect(existRoomByUuid(humanServiceRoomsStore, '3')).eq(false);
+    });
+
+    it('should transfer active room to other user', async () => {
+      const routerReplace = vi.fn();
+
+      humanServiceRoomsStore.$patch({
+        activeRoom: { ...humanServiceRoomsStore.rooms[0] },
+      });
+
+      const roomUuid = humanServiceRoomsStore.activeRoom.uuid;
+
+      await updateRoom(
+        {
+          ...humanServiceRoomsStore.activeRoom,
+          user: { email: 'testing-adm@weni.ai' },
+        },
+        {
+          app: {
+            ...humanServiceProfileStore,
+            viewedAgent,
+            $router: { replace: routerReplace },
+          },
+        },
+      );
+
+      expect(humanServiceRoomsStore.activeRoom).eq(null);
+      expect(routerReplace).toHaveBeenCalled();
+      expect(existRoomByUuid(humanServiceRoomsStore, roomUuid)).eq(false);
     });
   });
 });
