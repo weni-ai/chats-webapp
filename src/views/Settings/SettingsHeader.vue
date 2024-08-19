f533d4bd0d5e96dae1f9c9c5424c64e5a1a145aa
Aldemylla <aldemylla@gmail.com>
2024-08-19 19:26:33 -0300

    feat: Settings view header

---
+++ src/views/Settings/SettingsHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="settings-view__header">
+    <UnnnicAvatarIcon
+      icon="settings"
+      size="md"
+      scheme="weni-600"
+    />
+    <h1 class="header__title">{{ $t('config_chats.title') }}</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: 'SettingsHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+.settings-view__header {
+  display: flex;
+  align-items: center;
+  gap: $unnnic-spacing-xs;
+
+  .header__title {
+    color: $unnnic-color-neutral-darkest;
+    font-family: $unnnic-font-family-primary;
+    font-size: $unnnic-font-size-title-sm;
+    font-weight: $unnnic-font-weight-bold;
+    line-height: $unnnic-font-size-title-sm + $unnnic-line-height-md;
+  }
+}
+</style>
