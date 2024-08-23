55c53a7b98e7f3d71f6dc3a5f4af53916f94e8b2
Aldemylla <aldemylla@gmail.com>
2024-08-23 09:10:35 -0300

    feat: SectorEdit tests

---
+++ src/views/__tests__/SectorEdit.spec.js
@@ -0,0 +1,91 @@
+import { mount, flushPromises } from '@vue/test-utils';
+import { describe, it, expect, vi } from 'vitest';
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from '@/router/routes/settings';
+import { createTestingPinia } from '@pinia/testing';
+import SectorEdit from '@/views/Settings/Sectors/Edit/index.vue';
+
+const mockSector1 = {
+  id: 1,
+  uuid: '123',
+  name: 'Sector 1',
+  agents: 10,
+  contacts: 50,
+};
+
+const store = createTestingPinia({
+  initialState: {
+    config: {
+      project: { name: 'Project 1' },
+    },
+    settings: {
+      sectors: [mockSector1],
+      currentSector: mockSector1,
+      isLoadingSectors: false,
+    },
+  },
+});
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const createWrapper = (props) => {
+  return mount(SectorEdit, {
+    props,
+    global: {
+      plugins: [router, store],
+      stubs: {
+        UnnnicTab: true,
+      },
+      mocks: {
+        $router: {
+          replace: vi.fn(),
+        },
+      },
+    },
+  });
+};
+
+describe('EditSector.vue', () => {
+  let wrapper;
+
+  beforeEach(async () => {
+    router.push('/settings/sectors/1?tab=general');
+    wrapper = createWrapper({});
+    await flushPromises();
+  });
+
+  it('should render SectorEditHeader with the current sector name', async () => {
+    const viewHeader = wrapper.findComponent(
+      '[data-testid=sector-edit-view-header]',
+    );
+
+    expect(viewHeader.props('sectorName')).toBe('Sector 1');
+  });
+
+  it('should render UnnnicTab with the correct tabs', async () => {
+    const unnnicTab = wrapper.findComponent(
+      '[data-testid=sector-edit-view-tab-list]',
+    );
+    expect(unnnicTab.props('tabs')).toEqual(wrapper.vm.tabNames);
+  });
+
+  it('should update the active tab and router query on tab change', async () => {
+    const unnnicTab = wrapper.findComponent(
+      '[data-testid=sector-edit-view-tab-list]',
+    );
+    const spyRouterReplace = vi.spyOn(wrapper.vm.$router, 'replace');
+    await unnnicTab.vm.$emit('change', 'extra_options');
+
+    expect(wrapper.vm.activeTab.id).toBe('extra_options');
+    expect(spyRouterReplace).toHaveBeenCalledWith({
+      query: { tab: 'extra_options' },
+    });
+  });
+
+  it('Should match the snapshot', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
