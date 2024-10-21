ca5b8a158a0978214a0359e0a42152f894f6460a
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-10-21 18:05:58 -0300

    feat: add DiscussionsSession tests

--- setupVitest.js
+++ setupVitest.js
@@ -1,6 +1,21 @@
 import { config } from '@vue/test-utils';
 import i18n from '@/plugins/i18n.js';
 import UnnnicSystemPlugin from '@/plugins/UnnnicSystem.js';
+import { vi } from 'vitest';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
 
 config.global.plugins = [i18n, UnnnicSystemPlugin];
 config.global.mocks = {
