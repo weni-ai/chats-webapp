4c97a6071eaa317ab64612d8eedf14621a2ced1b
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-11-11 16:58:14 -0300

    fix: close method

--- src/views/chats/Home/ModalCloseChat.vue
+++ src/views/chats/Home/ModalCloseChat.vue
@@ -7,8 +7,7 @@
     :primaryButtonProps="{ text: $t('end'), loading: isLoadingCloseRoom }"
     data-testid="chat-classifier-modal"
     @primary-button-click="closeRoom()"
-    @secondary-button-click="closeModal()"
-    @close="closeModal()"
+    @update:model-value="closeModal()"
   >
     <ChatClassifier
       v-model="tags"
