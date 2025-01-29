2a6a30b168689e2408678398a699bacfa0e37a45
Marcus Vinícius <marcusvinici25@gmail.com>
2025-01-29 15:06:44 -0300

    fix: secondary-btn-modal-close-chat

--- src/views/chats/Home/ModalCloseChat.vue
+++ src/views/chats/Home/ModalCloseChat.vue
@@ -5,6 +5,7 @@
     :showCloseIcon="!isMobile"
     :title="$t('chats.to_end_rate_the_chat')"
     :primaryButtonProps="{ text: $t('end'), loading: isLoadingCloseRoom }"
+    :secondaryButtonProps="{ text: $t('cancel') }"
     size="lg"
     data-testid="chat-classifier-modal"
     @primary-button-click="closeRoom()"
