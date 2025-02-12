46d9f931c38d509c84cdd7cf461f88cb5be91f0e
Marcus Vinícius <marcusvinici25@gmail.com>
2025-02-12 13:47:04 -0300

    adding options and styles

--- src/layouts/ChatsLayout/components/ViewOptions/index.vue
+++ src/layouts/ChatsLayout/components/ViewOptions/index.vue
@@ -20,6 +20,55 @@
         :handleClick="closeDrawer"
         :expandedMore="isOpen"
       />
+      <section class="drawer__options">
+        <section class="drawer__option">
+          <UnnnicSwitch
+            data-testid="switch-sound"
+            size="small"
+            :textRight="$t('preferences.notifications.sound')"
+          />
+        </section>
+
+        <section class="drawer__option">
+          <UnnnicButton
+            data-testid="show-flows-trigger"
+            :text="$t('flows')"
+            size="small"
+            type="tertiary"
+            iconLeft="send"
+            @mousedown.prevent
+          />
+        </section>
+        <section class="drawer__option">
+          <UnnnicButton
+            data-testid="show-quick-messages"
+            :text="$t('quick_messages.title')"
+            iconLeft="bolt"
+            type="tertiary"
+            size="small"
+            @mousedown.prevent
+          />
+        </section>
+        <section class="drawer__option">
+          <UnnnicButton
+            data-testid="show-dashboard"
+            text="Dashboard"
+            iconLeft="bar_chart_4_bars"
+            type="tertiary"
+            size="small"
+            @mousedown.prevent
+          />
+        </section>
+        <section class="drawer__option">
+          <UnnnicButton
+            class="chats-layout-footer-button__button"
+            :text="$t('chats.see_history')"
+            iconLeft="history"
+            type="tertiary"
+            size="small"
+          />
+        </section>
+      </section>
     </div>
   </Teleport>
 </template>
@@ -58,7 +107,6 @@ section.chats-layout .sidebar .footer {
   position: fixed;
   bottom: -(calc(100vw * (3 / 12)));
   width: calc(100vw * (3 / 12));
-  height: 22vh;
   transition: bottom 0.3s ease-in-out;
   z-index: 1000;
 
@@ -66,6 +114,45 @@ section.chats-layout .sidebar .footer {
   border-bottom: 1px solid $unnnic-color-neutral-soft;
   border-left: 1px solid $unnnic-color-neutral-soft;
   background-color: $unnnic-color-neutral-lightest;
+
+  &__option {
+    width: 100%;
+    :hover {
+      background-color: $unnnic-color-neutral-light;
+    }
+    :deep(.unnnic-switch) {
+      padding: $unnnic-spacing-nano + 1px $unnnic-spacing-sm;
+      width: 100%;
+      cursor: pointer;
+    }
+
+    :deep(.unnnic-switch__label__small) {
+      color: $unnnic-color-neutral-cloudy;
+
+      font-family: $unnnic-font-family-secondary;
+      font-size: $unnnic-font-size-body-gt;
+      font-style: normal;
+      font-weight: $unnnic-font-weight-regular;
+      line-height: $unnnic-font-size-body-gt + $unnnic-line-height-md;
+    }
+
+    :deep(.unnnic-button--size-small) {
+      padding: 0 $unnnic-spacing-sm;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: start;
+    }
+
+    :deep(.material-symbols-rounded.unnnic-icon-size--sm) {
+      font-size: $unnnic-avatar-size-nano;
+      color: $unnnic-color-neutral-cloudy;
+    }
+
+    :deep(.unnnic-button__label) {
+      color: $unnnic-color-neutral-cloudy;
+    }
+  }
 }
 
 .drawer.open {
