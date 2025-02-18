93f1aa1bf16cd85a1935414c25c78532daf25dfa
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2025-02-18 16:07:11 -0300

    wip: general sector form tests

--- src/components/settings/forms/__tests__/Sector.spec.js
+++ src/components/settings/forms/__tests__/Sector.spec.js
@@ -10,6 +10,7 @@ import defaultProps from './mocks/sectorMock';
 
 import Sector from '@/services/api/resources/settings/sector';
 import Project from '@/services/api/resources/settings/project';
+import Group from '@/services/api/resources/settings/group';
 
 const pinia = createTestingPinia({
   initialState: {
@@ -31,9 +32,21 @@ const managerMock = {
   },
 };
 
+const projectMock = { uuid: '1', name: 'projectMock' };
+
 vi.spyOn(Project, 'managers').mockResolvedValue({
   results: [managerMock],
 });
+
+vi.spyOn(Group, 'listProjects').mockResolvedValue({
+  results: [projectMock],
+});
+
+vi.mock('@/services/api/resources/settings/group', () => ({
+  default: {
+    listProjects: vi.fn(),
+  },
+}));
 
 vi.mock('@/services/api/resources/settings/sector', () => ({
   default: {
@@ -371,5 +384,39 @@ describe('FormSectorGeneral', () => {
     await wrapper.setProps({ isEditing: true });
     await wrapper.vm.removeManager('1');
     expect(removeManagerSpy).toHaveBeenCalledWith('1');
+  });
+
+  it('should return the list of project names', async () => {
+    await wrapper.setData({
+      projects: [
+        { uuid: '1', name: 'Project One' },
+        { uuid: '2', name: 'Project Two' },
+      ],
+    });
+
+    const result = wrapper.vm.projectsNames;
+
+    expect(result).toEqual([
+      { value: '', label: wrapper.vm.$t('sector.managers.add.placeholder') },
+      { value: '1', label: 'Project One' },
+      { value: '2', label: 'Project Two' },
+    ]);
+  });
+
+  it('should return the list of secondary projects', async () => {
+    await wrapper.vm.listSecondaryProjects();
+    expect(wrapper.vm.projects).toEqual([projectMock]);
+  });
+
+  it('should set selected project and clear secondary projects', async () => {
+    await wrapper.vm.listSecondaryProjects();
+
+    wrapper.vm.selectProject([wrapper.vm.projectsNames[1]]);
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.sector.config.integration_token).toBe(
+      wrapper.vm.projectsNames[1].value,
+    );
   });
 });
