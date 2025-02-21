391a57458e678e8fccc3fd77f52e927be82d5702
Marcus Vinícius <marcusvinici25@gmail.com>
2025-02-20 23:42:41 -0300

    adding new animation logic and clickOutside

--- src/layouts/ChatsLayout/components/ViewOptions/index.vue
+++ src/layouts/ChatsLayout/components/ViewOptions/index.vue
@@ -11,91 +11,92 @@
     to="body"
     data-testid="teleport-body"
   >
-    <section
-      v-if="isOpen"
-      class="chats-layout-drawer__overlay"
-      data-testid="drawer-overlay"
-      @click="closeDrawer"
-    ></section>
-    <section
-      class="chats-layout-drawer"
-      :class="{ 'chats-layout-drawer--open': isOpen }"
-      data-testid="drawer"
-    >
-      <ViewButton
-        :handleClick="closeDrawer"
-        :expandedMore="isOpen"
-        data-testid="view-btn-drawer"
-      />
-      <section class="chats-layout-drawer__options">
-        <section class="chats-layout-drawer__option">
-          <UnnnicSwitch
-            v-if="!isViewMode"
-            v-model="sound"
-            data-testid="switch-sound"
-            size="small"
-            :textRight="$t('preferences.notifications.sound')"
-            @update:model-value="changeSound"
-          />
-        </section>
+    <Transition name="expand">
+      <section
+        v-if="isOpen"
+        class="chats-layout-drawer"
+        :class="{ 'chats-layout-drawer--open': isOpen }"
+        data-testid="drawer"
+      >
+        <ViewButton
+          :handleClick="closeDrawer"
+          :expandedMore="isOpen"
+          data-testid="view-btn-drawer"
+        />
+        <section class="chats-layout-drawer__options">
+          <section class="chats-layout-drawer__option">
+            <UnnnicSwitch
+              v-if="!isViewMode"
+              v-model="sound"
+              data-testid="switch-sound"
+              size="small"
+              :textRight="$t('preferences.notifications.sound')"
+              @update:model-value="changeSound"
+            />
+          </section>
 
-        <section class="chats-layout-drawer__option">
-          <UnnnicButton
-            v-if="showFlowsTriggerButton || !isViewMode"
-            data-testid="show-flows-trigger"
-            :text="$t('flows')"
-            size="small"
-            type="tertiary"
-            iconLeft="send"
-            @mousedown.prevent
-            @click="openFlowsTrigger"
-          />
-        </section>
-        <section class="chats-layout-drawer__option">
-          <UnnnicButton
-            v-if="!isViewMode"
-            data-testid="show-quick-messages"
-            :text="$t('quick_messages.title')"
-            iconLeft="bolt"
-            type="tertiary"
-            size="small"
-            @mousedown.prevent
-            @click="openQuickMessage"
-          />
-        </section>
-        <section class="chats-layout-drawer__option">
-          <UnnnicButton
-            v-if="dashboard || !isViewMode"
-            data-testid="show-dashboard"
-            text="Dashboard"
-            iconLeft="bar_chart_4_bars"
-            type="tertiary"
-            size="small"
-            @mousedown.prevent
-            @click="navigate('dashboard.manager')"
-          />
-        </section>
-        <section class="chats-layout-drawer__option">
-          <UnnnicButton
-            class="chats-layout-drawer__button"
-            :text="$t('chats.see_history')"
-            iconLeft="history"
-            type="tertiary"
-            size="small"
-            data-testid="show-see_history"
-            @click="navigate('closed-rooms')"
-          />
+          <section class="chats-layout-drawer__option">
+            <UnnnicButton
+              v-if="showFlowsTriggerButton || !isViewMode"
+              data-testid="show-flows-trigger"
+              :text="$t('flows')"
+              size="small"
+              type="tertiary"
+              iconLeft="send"
+              @mousedown.prevent
+              @click="openFlowsTrigger"
+            />
+          </section>
+          <section class="chats-layout-drawer__option">
+            <UnnnicButton
+              v-if="!isViewMode"
+              data-testid="show-quick-messages"
+              :text="$t('quick_messages.title')"
+              iconLeft="bolt"
+              type="tertiary"
+              size="small"
+              @mousedown.prevent
+              @click="openQuickMessage"
+            />
+          </section>
+          <section class="chats-layout-drawer__option">
+            <UnnnicButton
+              v-if="dashboard || !isViewMode"
+              data-testid="show-dashboard"
+              text="Dashboard"
+              iconLeft="bar_chart_4_bars"
+              type="tertiary"
+              size="small"
+              @mousedown.prevent
+              @click="navigate('dashboard.manager')"
+            />
+          </section>
+          <section class="chats-layout-drawer__option">
+            <UnnnicButton
+              class="chats-layout-drawer__button"
+              :text="$t('chats.see_history')"
+              iconLeft="history"
+              type="tertiary"
+              size="small"
+              data-testid="show-see_history"
+              @click="navigate('closed-rooms')"
+            />
+          </section>
         </section>
       </section>
-    </section>
+    </Transition>
   </Teleport>
 </template>
 
 <script setup>
-import { ref, onBeforeMount, defineEmits } from 'vue';
+import { ref, onBeforeMount, defineEmits, onMounted, onUnmounted } from 'vue';
 import ViewButton from './ViewButton.vue';
 import { PREFERENCES_SOUND } from '@/services/api/websocket/soundNotification.js';
 import { useRouter } from 'vue-router';
+
+defineOptions({
+  name: 'ViewOptions',
+});
 
 defineProps({
   showFlowsTriggerButton: {
@@ -118,6 +119,15 @@ const { push } = useRouter();
 
 const isOpen = ref(false);
 const sound = ref(false);
+
+const handleClickOutside = (event) => {
+  const drawer = event.target.closest('[data-testid="drawer"]');
+  const viewButton = event.target.closest('[data-testid="view-btn"]');
+
+  if (!drawer && !viewButton && isOpen.value) {
+    isOpen.value = false;
+  }
+};
 
 const closeDrawer = () => {
   isOpen.value = false;
@@ -148,22 +158,20 @@ const navigate = (name) => {
 onBeforeMount(() => {
   sound.value = (localStorage.getItem(PREFERENCES_SOUND) || 'yes') === 'yes';
 });
+
+onMounted(async () => {
+  document.addEventListener('click', handleClickOutside);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleClickOutside);
+});
 </script>
 
 <style lang="scss" scoped>
-.chats-layout-drawer__overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-
 .chats-layout-drawer {
   position: fixed;
-  bottom: -(calc(100vw * (3 / 12)));
   width: calc(100vw * (3 / 12));
-  transition: bottom 0.3s ease-in-out;
   z-index: 1000;
 
   border-top: 1px solid $unnnic-color-neutral-soft;
@@ -219,5 +227,25 @@ onBeforeMount(() => {
 
 section.chats-layout .sidebar .chats-layout-footer {
   padding-right: 0;
+}
+
+.expand-enter-active,
+.expand-leave-active {
+  transition: all 0.3s ease-out;
+  max-height: calc(100vw * (3 / 12));
+}
+
+.expand-enter-from,
+.expand-leave-to {
+  max-height: 0;
+  opacity: 0;
+  overflow: hidden;
+}
+
+.expand-enter-to,
+.expand-leave-from {
+  max-height: calc(100vw * (3 / 12));
+  opacity: 1;
+  overflow: hidden;
 }
 </style>
