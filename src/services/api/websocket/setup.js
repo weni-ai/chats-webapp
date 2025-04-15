cca42ca5750cb40129ec2c33be20f67c0138e680
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2025-04-15 11:41:16 -0300

    fix: add timeout to reconnect socket

--- src/services/api/websocket/setup.js
+++ src/services/api/websocket/setup.js
@@ -6,6 +6,7 @@ import { useDashboard } from '@/store/modules/dashboard';
 
 export default class WebSocketSetup {
   THIRTY_SECONDS = 30000;
+  FIVE_SECONDS = 5000;
 
   constructor({ app }) {
     this.app = app;
@@ -33,12 +34,14 @@ export default class WebSocketSetup {
     this.ws = ws;
     this.ws.ws.onclose = () => {
       if (this.ws.ws.readyState === this.ws.ws.OPEN) return;
-      const timestamp = new Date().toISOString();
-      console.warn(
-        timestamp,
-        '[WebSocket] Connection closed, reconnecting immediately...',
-      );
-      this.reconnect();
+      setTimeout(() => {
+        const timestamp = new Date().toISOString();
+        console.warn(
+          timestamp,
+          '[WebSocket] Connection closed, reconnecting immediately...',
+        );
+        this.reconnect();
+      }, this.FIVE_SECONDS);
     };
     listeners({ ws, app: this.app });
     this.setupPingInterval();
