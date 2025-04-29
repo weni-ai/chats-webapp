5fc9616ceab7c6be9c3459bdb0bd4461ccf465fe
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2025-04-29 10:45:40 -0300

    fix: last message not update

--- src/services/api/websocket/listeners/room/message/create.js
+++ src/services/api/websocket/listeners/room/message/create.js
@@ -9,6 +9,7 @@ export default async (message, { app }) => {
   const roomsStore = useRooms();
   const roomMessagesStore = useRoomMessages();
   const { rooms, activeRoom } = roomsStore;
+
   const findRoom = rooms.find((room) => room.uuid === message.room);
 
   roomsStore.bringRoomFront(findRoom);
@@ -42,17 +43,16 @@ export default async (message, { app }) => {
       roomMessagesStore.addMessage(message);
     }
 
-    if (isValidJson(message.text)) return;
-
-    roomsStore.addNewMessagesByRoom({
-      room: message.room,
-      message: {
-        created_on: message.created_on,
-        uuid: message.uuid,
-        text: message.text,
-      },
-    });
-
-    findRoom.last_message = message;
+    if (!isValidJson(message.text)) {
+      findRoom.last_message = message;
+      roomsStore.addNewMessagesByRoom({
+        room: message.room,
+        message: {
+          created_on: message.created_on,
+          uuid: message.uuid,
+          text: message.text,
+        },
+      });
+    }
   }
 };
