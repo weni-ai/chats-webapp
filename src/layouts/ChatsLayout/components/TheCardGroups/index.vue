ce6d32e45e2ff231bc5afc4b0413111ffaf15456
Marcus Vinícius <marcusvinici25@gmail.com>
2025-06-16 10:27:28 -0300

    fix: add optional chaining to error response status checks in TheCardGroups component

--- src/layouts/ChatsLayout/components/TheCardGroups/index.vue
+++ src/layouts/ChatsLayout/components/TheCardGroups/index.vue
@@ -393,13 +393,13 @@ export default {
         console.error('Pin room error', error);
         let errorText = '';
 
-        if (error.response.status === 401) {
+        if (error.response?.status === 401) {
           errorText = this.$t('chats.errors.401');
-        } else if (error.response.status === 403) {
+        } else if (error.response?.status === 403) {
           errorText = this.$t('chats.room_pin.error_403');
-        } else if (error.response.status === 404) {
+        } else if (error.response?.status === 404) {
           errorText = this.$t('chats.room_pin.error_404');
-        } else if (error.response.status === 400) {
+        } else if (error.response?.status === 400) {
           errorText = this.$t('chats.room_pin.error_pin_limit', {
             max_pin_limit: this.maxPinLimit,
           });
