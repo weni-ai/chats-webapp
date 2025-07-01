cd445aecee65cc09c237fc8116ac26b8f3e02e61
Marcus Vinícius <marcusvinici25@gmail.com>
2025-07-01 18:36:05 -0300

    fix: enhance session storage key handling for user status updates with project UUID fallback

--- src/services/api/websocket/listeners/status/update.js
+++ src/services/api/websocket/listeners/status/update.js
@@ -1,7 +1,13 @@
 import { useConfig } from '@/store/modules/config';
 
 export default (content, { app }) => {
-  const sessionStorageStatus = sessionStorage.getItem('statusAgent');
+  const configStore = useConfig();
+
+  const statusAgentKey = configStore.project.uuid
+    ? `statusAgent-${configStore.project.uuid}`
+    : `statusAgent-${sessionStorage.getItem('WENICHATS_PROJECT_UUID')}`;
+
+  const sessionStorageStatus = sessionStorage.getItem(statusAgentKey);
   const { from, status } = content;
 
   if (sessionStorageStatus !== status) {
@@ -10,7 +16,7 @@ export default (content, { app }) => {
     } else if (from === 'user') {
       const configStore = useConfig();
       configStore.setStatus(status);
-      sessionStorage.setItem('statusAgent', status);
+      sessionStorage.setItem(statusAgentKey, status);
     }
   }
 };
