6537ed9e90c057be36881ae5deb141926d9888f7
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-01 17:24:03 -0300

    feat: add ModalOfflineAgent component for offline user feedback
    - Introduced a new ModalOfflineAgent.vue component to provide users with feedback when offline. The modal includes a description and utilizes localization for the title and content.

---
+++ src/components/ModalOfflineAgent.vue
@@ -0,0 +1,36 @@
+<template>
+  <UnnnicModalDialog
+    :modelValue="modelValue"
+    class="modal-offline-agent"
+    showCloseIcon
+    :title="$t('offline_agent_modal.title')"
+    size="lg"
+    data-testid="offline-agent-modal"
+    @update:model-value="closeModal()"
+  >
+    <section class="modal-offline-agent__content">
+      <p class="modal-offline-agent__content__description">
+        {{ $t('offline_agent_modal.description', { username: 'John Doe' }) }}
+      </p>
+    </section>
+  </UnnnicModalDialog>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const modelValue = ref(false);
+
+const closeModal = () => {
+  modelValue.value = false;
+};
+</script>
+
+<style lang="scss" scoped>
+.modal-offline-agent {
+  &__content {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
