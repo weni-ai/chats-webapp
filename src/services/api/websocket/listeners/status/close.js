9b4ab626bf77077dca484b40f5e7de0c2b72f274
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-02 21:14:17 -0300

    feat: update status close listener to handle user disconnection
    - Modified the status close listener to check for user disconnection and update the user's status accordingly.
    - Enhanced the listener's functionality by utilizing the user_disconnected_agent property from the content object.

--- src/services/api/websocket/listeners/status/close.js
+++ src/services/api/websocket/listeners/status/close.js
@@ -1,5 +1,7 @@
 export default (content, { app }) => {
-  const { from, status } = content;
+  const { status, user_disconnected_agent } = content;
 
-  console.log('status close', content);
+  if (user_disconnected_agent && status) {
+    app.updateUserStatusFromWebSocket(status, user_disconnected_agent);
+  }
 };
