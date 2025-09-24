530b0b6696e22a146f3ecd74e9081602513f0448
Marcus Vinícius <marcusvinici25@gmail.com>
2025-09-24 09:49:33 -0300

    feat: add websocket listener for user disconnection status
    - Implemented a new listener in the websocket service to update user status to 'OFFLINE' when an agent disconnects. This enhances real-time user status management in the application.

---
+++ src/services/api/websocket/listeners/customStatus/close.js
@@ -0,0 +1,7 @@
+export default (content, { app }) => {
+  const { user_disconnected_agent } = content;
+
+  if (user_disconnected_agent) {
+    app.updateUserStatusFromWebSocket('OFFLINE', user_disconnected_agent);
+  }
+};
