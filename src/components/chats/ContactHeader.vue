09741f703a3753fb2de8d2f1eea6503c922d2fce
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2026-01-14 11:07:58 -0300

    Update ContactHeader component dimensions for improved UI consistency

--- src/components/chats/ContactHeader.vue
+++ src/components/chats/ContactHeader.vue
@@ -68,8 +68,8 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 35px;
-    height: 35px;
+    width: 37px;
+    height: 37px;
     background-color: $unnnic-color-gray-200;
 
     font: $unnnic-font-action;
