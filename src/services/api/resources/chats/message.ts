048a14cf190e5c523f40680f608e83941ed118bb
Marcus Vinícius <marcusvinici25@gmail.com>
2026-03-06 10:01:19 -0300

    Update src/services/api/resources/chats/message.ts
    Co-authored-by: Eduardo Medeiros <eduardomedeirosrn2@gmail.com>

--- src/services/api/resources/chats/message.ts
+++ src/services/api/resources/chats/message.ts
@@ -164,7 +164,7 @@ export default {
         replied_message_id: repliedMessageId,
       },
       {
-        onUploadProgress: (event: any) => {
+        onUploadProgress: (event: ProgressEvent) => {
           const progress = event.loaded / event.total;
           updateLoadingFiles?.(msg.uuid, progress);
         },
