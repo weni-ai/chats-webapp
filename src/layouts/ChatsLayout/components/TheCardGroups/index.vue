b78501eb4ba3674955eb9aaaecdb4315769a3722
Marcus Vinícius <marcusvinici25@gmail.com>
2026-03-12 18:12:11 -0300

    feat: Add bulk transfer feature toggle to ChatsLayout
    - Implemented a method to check if the bulk transfer feature is enabled.
    - Updated logic in the waiting tab to include bulk transfer capability alongside existing bulk take and close options.

--- src/layouts/ChatsLayout/components/TheCardGroups/index.vue
+++ src/layouts/ChatsLayout/components/TheCardGroups/index.vue
@@ -338,6 +338,12 @@ export default {
       return this.featureFlags.active_features?.includes('weniChatsBulkTake');
     },
 
+    isBulkTransferFeatureEnabled() {
+      return this.featureFlags.active_features?.includes(
+        'weniChatsBulkTransfer',
+      );
+    },
+
     showSelectAllCheckbox() {
       const canBulkTransfer = this.project.config?.can_use_bulk_transfer;
       const canBulkClose =
@@ -351,8 +357,13 @@ export default {
       const hasRooms = this.countRooms[this.activeTab] > 0;
 
       if (this.activeTab === 'waiting') {
+        const canBulkTransferInWaiting =
+          this.isBulkTransferFeatureEnabled && canBulkTransfer;
         return (
-          hasRooms && (canBulkTake || (canBulkClose && !blockCloseInQueue))
+          hasRooms &&
+          (canBulkTake ||
+            canBulkTransferInWaiting ||
+            (canBulkClose && !blockCloseInQueue))
         );
       }
 
@@ -427,7 +438,13 @@ export default {
       if (this.isMobile) return false;
 
       if (this.activeTab === 'waiting') {
-        return canBulkTake || (canBulkClose && !blockCloseInQueue);
+        const canBulkTransferInWaiting =
+          this.isBulkTransferFeatureEnabled && canBulkTransfer;
+        return (
+          canBulkTake ||
+          canBulkTransferInWaiting ||
+          (canBulkClose && !blockCloseInQueue)
+        );
       }
 
       return canBulkTransfer || canBulkClose;
