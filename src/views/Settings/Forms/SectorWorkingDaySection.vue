3dec96b581a8b9999ee2bf9637097f06262f4b41
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2026-05-04 17:50:13 -0300

    refactor: enhance country name display in SectorWorkingDaySection and CountryHolidaysModal
    - Added a check in the computed property `countryName` to return a default label when `countryCode` is not set, improving user experience and localization handling in both components.

--- src/views/Settings/Forms/SectorWorkingDaySection.vue
+++ src/views/Settings/Forms/SectorWorkingDaySection.vue
@@ -355,6 +355,7 @@ export default {
       };
     },
     countryName() {
+      if (!this.countryCode) return this.$t('country.label');
       const locale = this.$i18n.locale;
       return new Intl.DisplayNames(locale, { type: 'region' }).of(
         this.countryCode,
