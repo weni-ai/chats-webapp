ae755964dc50efebf5a48213d6518fea7668198c
Marcus Vinícius <marcusvinici25@gmail.com>
2026-05-19 09:33:34 -0300

    fix: prevent new chat received indicator from displaying in certain conditions
    - Updated RoomCard component to ensure the new chat received indicator does not show when the showNewChatReceivedIndicator flag is false.
    - Enhanced logic for displaying the indicator based on the state of the ongoing room and message validity.

--- src/layouts/ChatsLayout/components/TheCardGroups/CardGroup/RoomCard.vue
+++ src/layouts/ChatsLayout/components/TheCardGroups/CardGroup/RoomCard.vue
@@ -156,6 +156,7 @@ export default {
         this.isLastMessageFromBot;
 
       return (
+        !this.showNewChatReceivedIndicator &&
         this.isPendingResponseFeatureEnabled &&
         this.isProgressRoom &&
         isLastMessageValid &&
