d0423862d3df0a4ec0a479cb164ac5f2011ae6bf
Mateus Eduardo Batista de Medeiros <eduardomedeirosrn2@gmail.com>
2026-05-22 09:14:11 -0300

    feat: add inactivity listener to update room status
    - Introduced a new inactivity listener to update the active status of rooms based on incoming data.
    - Integrated the listener into the existing room management module for better handling of room activity states.

---
+++ src/services/api/websocket/listeners/room/inactivity.js
@@ -0,0 +1,11 @@
+import { useRooms } from '@/store/modules/chats/rooms';
+
+export default (data, _ctx) => {
+  const roomsStore = useRooms();
+
+  const existingRoom = roomsStore.rooms.find((r) => r.uuid === data.room_uuid);
+
+  if (!existingRoom) return;
+
+  existingRoom.is_active = data.is_inactive;
+};
