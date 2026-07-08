72671aefc28c326b29f08ed8073b49320287e098
Marcus Vinícius <marcusvinici25@gmail.com>
2026-07-07 22:12:47 -0300

    feat: add additional query parameters for chat history retrieval

--- src/services/api/resources/chats/history.js
+++ src/services/api/resources/chats/history.js
@@ -8,22 +8,29 @@ export default {
     ended_at_before = '',
     ended_at_after = '',
     search = '',
+    contact = '',
+    email = '',
+    document = '',
     sector = '',
     tag = '',
   }) {
-    const response = await http.get(`/history/rooms/`, {
-      params: {
-        project: getProject(),
-        offset,
-        limit,
-        ended_at_before,
-        ended_at_after,
-        sector,
-        search,
-        tag,
-        ordering: '-ended_at',
-      },
-    });
+    const params = {
+      project: getProject(),
+      offset,
+      limit,
+      ended_at_before,
+      ended_at_after,
+      sector,
+      tag,
+      ordering: '-ended_at',
+    };
+
+    if (search) params.search = search;
+    if (contact) params.contact = contact;
+    if (email) params.email = email;
+    if (document) params.document = document;
+
+    const response = await http.get(`/history/rooms/`, { params });
     return response.data;
   },
 
